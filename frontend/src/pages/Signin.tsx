47cc239c6f7709aa1896fe4f885bf18fd1deae0f
prajwal-dahat0810 <dahatprajwal11@gmail.com>
2026-01-02 22:22:28 +0530

    fixed signin ui bugs

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -168,151 +168,130 @@ export default function () {
   }
 
   return (
-    <div className="min-h-[100vh]  relative bg-[#323437] flex flex-col flex-grow justify-between  max-h-full w-full items-center  ">
-      {/* <Toaster
-        id="smallScreen"
-        toastOptions={{
-          classNames: {
-            toast: "!bg-[#323437] !border-2 !border[#6cb4ee]",
-            success: "!bg-[#7FD88B]/9 !border-2 !border-[#7fd88b] !text-white ",
-            error: "!bg-[#FF6B6B]/9 !border-2 !border-[#ff6b6b] !text-white",
-            loading: "!bg-[#545f69]/9 !border-2 !border-[#6CB4EE] !text-white",
-          },
-        }}
-        position="top-center"
-      />
-      <Toaster
-        id="bigScreen"
-        toastOptions={{
-          classNames: {
-            toast: "!bg-[#323437] !border-2 !border[#6cb4ee]",
-            success: "!bg-[#7FD88B]/9 !border-2 !border-[#7fd88b] !text-white ",
-            error: "!bg-[#FF6B6B]/9 !border-2 !border-[#ff6b6b] !text-white",
-            loading: "!bg-[#545f69]/9 !border-2 !border-[#6CB4EE] !text-white",
-          },
-        }}
-      /> */}
-      <div className=" min-h-80  max-sm:min-h-max grid col-span-full my-4   w-full max-w-5xl">
-        <div className="col-start-1 max-sm:mt-10 col-span-1 max-sm:col-start-1 max-sm:row-start-2 max-sm:col-span-2 flex flex-col  max-w-xl items-center justify-center ">
-          <div className="flex  gap-3 py-4   justify-center items-center max-w-[300px] w-full  flex-col">
-            <div className="w-full text-[#646669] px-2 flex justify-start items-center  gap-1 font-mono ">
-              <div className="">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="size-5"
-                >
-                  <path d="M10 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM1.615 16.428a1.224 1.224 0 0 1-.569-1.175 6.002 6.002 0 0 1 11.908 0c.058.467-.172.92-.57 1.174A9.953 9.953 0 0 1 7 18a9.953 9.953 0 0 1-5.385-1.572ZM16.25 5.75a.75.75 0 0 0-1.5 0v2h-2a.75.75 0 0 0 0 1.5h2v2a.75.75 0 0 0 1.5 0v-2h2a.75.75 0 0 0 0-1.5h-2v-2Z" />
-                </svg>
+    <div className="min-h-[100vh]   bg-[#323437] flex flex-col  flex-grow justify-center  max-h-full w-full items-center  ">
+      <div className="   bg-[#323437] flex  flex-grow justify-center  h-full w-full items-center  ">
+        <div className=" min-h-80  max-sm:min-h-max grid col-span-full my-4  w-full   max-w-5xl">
+          <div className="col-start-1 max-sm:mt-10 col-span-1 max-sm:col-start-1 max-sm:row-start-2 max-sm:col-span-2 flex flex-col  max-w-xl items-center justify-center ">
+            <div className="flex  gap-3 py-4   justify-center items-center max-w-[300px] w-full  flex-col">
+              <div className="w-full text-[#646669] px-2 flex justify-start items-center  gap-1 font-mono ">
+                <div className="">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path d="M10 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM1.615 16.428a1.224 1.224 0 0 1-.569-1.175 6.002 6.002 0 0 1 11.908 0c.058.467-.172.92-.57 1.174A9.953 9.953 0 0 1 7 18a9.953 9.953 0 0 1-5.385-1.572ZM16.25 5.75a.75.75 0 0 0-1.5 0v2h-2a.75.75 0 0 0 0 1.5h2v2a.75.75 0 0 0 1.5 0v-2h2a.75.75 0 0 0 0-1.5h-2v-2Z" />
+                  </svg>
+                </div>
+                <div>register</div>
               </div>
-              <div>register</div>
+              <input
+                type="text"
+                aria-label=""
+                className="w-full text-[#d1d0c5]  focus:outline-2 focus:outline-amber-200   px-2 py-2  font-mono placeholder:text-[#696a6c] bg-[#2C2E31] p-1 rounded-sm "
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="username"
+                value={userName}
+              />
+              <input
+                type="text"
+                aria-label=""
+                value={signupEmail}
+                className="w-full text-[#d1d0c5]  focus:outline-2 focus:outline-amber-200    px-2 py-2  font-mono placeholder:text-[#696a6c] bg-[#2C2E31] p-1 rounded-sm "
+                onChange={(e) => setSignupEmail(e.target.value)}
+                placeholder="email"
+              />
+              <input
+                type="text"
+                aria-label=""
+                className="w-full text-[#d1d0c5]  focus:outline-2 focus:outline-amber-200    px-2 py-2  font-mono placeholder:text-[#696a6c] bg-[#2C2E31] p-1 rounded-sm "
+                onChange={(e) => setSignupPassword(e.target.value)}
+                placeholder="password"
+                value={signupPassword}
+              />
+              <button
+                onClick={handleSignup}
+                className="w-full cursor-pointer hover:bg-[#d1d0c5] hover:text-[#323437] py-1.5  text-[#646669] flex justify-center items-center gap-2   px-2   font-mono  bg-[#35383c] p-1 rounded-sm "
+              >
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path d="M10 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM1.615 16.428a1.224 1.224 0 0 1-.569-1.175 6.002 6.002 0 0 1 11.908 0c.058.467-.172.92-.57 1.174A9.953 9.953 0 0 1 7 18a9.953 9.953 0 0 1-5.385-1.572ZM16.25 5.75a.75.75 0 0 0-1.5 0v2h-2a.75.75 0 0 0 0 1.5h2v2a.75.75 0 0 0 1.5 0v-2h2a.75.75 0 0 0 0-1.5h-2v-2Z" />
+                  </svg>
+                </div>{" "}
+                sign up
+              </button>
             </div>
-            <input
-              type="text"
-              aria-label=""
-              className="w-full text-[#d1d0c5]  focus:outline-2 focus:outline-amber-200   px-2 py-2  font-mono placeholder:text-[#696a6c] bg-[#2C2E31] p-1 rounded-sm "
-              onChange={(e) => setUsername(e.target.value)}
-              placeholder="username"
-              value={userName}
-            />
-            <input
-              type="text"
-              aria-label=""
-              value={signupEmail}
-              className="w-full text-[#d1d0c5]  focus:outline-2 focus:outline-amber-200    px-2 py-2  font-mono placeholder:text-[#696a6c] bg-[#2C2E31] p-1 rounded-sm "
-              onChange={(e) => setSignupEmail(e.target.value)}
-              placeholder="email"
-            />
-            <input
-              type="text"
-              aria-label=""
-              className="w-full text-[#d1d0c5]  focus:outline-2 focus:outline-amber-200    px-2 py-2  font-mono placeholder:text-[#696a6c] bg-[#2C2E31] p-1 rounded-sm "
-              onChange={(e) => setSignupPassword(e.target.value)}
-              placeholder="password"
-              value={signupPassword}
-            />
-            <button
-              onClick={handleSignup}
-              className="w-full cursor-pointer hover:bg-[#d1d0c5] hover:text-[#323437] py-1.5  text-[#646669] flex justify-center items-center gap-2   px-2   font-mono  bg-[#35383c] p-1 rounded-sm "
-            >
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="size-5"
-                >
-                  <path d="M10 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM1.615 16.428a1.224 1.224 0 0 1-.569-1.175 6.002 6.002 0 0 1 11.908 0c.058.467-.172.92-.57 1.174A9.953 9.953 0 0 1 7 18a9.953 9.953 0 0 1-5.385-1.572ZM16.25 5.75a.75.75 0 0 0-1.5 0v2h-2a.75.75 0 0 0 0 1.5h2v2a.75.75 0 0 0 1.5 0v-2h2a.75.75 0 0 0 0-1.5h-2v-2Z" />
-                </svg>
-              </div>{" "}
-              sign up
-            </button>
           </div>
-        </div>
-        <div className="col-start-2 max-sm:mt-36 max-sm:col-start-1 max-sm:row-start-3 max-sm:col-span-2  col-span-1 flex flex-col  max-w-xl items-center justify-center ">
-          <div className="flex  gap-3 py-4   justify-center items-center max-w-[300px] w-full  flex-col">
-            <div className="w-full text-[#646669] px-2 flex justify-start items-center  gap-3 font-mono ">
-              <div className="">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="size-5"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M17 4.25A2.25 2.25 0 0 0 14.75 2h-5.5A2.25 2.25 0 0 0 7 4.25v2a.75.75 0 0 0 1.5 0v-2a.75.75 0 0 1 .75-.75h5.5a.75.75 0 0 1 .75.75v11.5a.75.75 0 0 1-.75.75h-5.5a.75.75 0 0 1-.75-.75v-2a.75.75 0 0 0-1.5 0v2A2.25 2.25 0 0 0 9.25 18h5.5A2.25 2.25 0 0 0 17 15.75V4.25Z"
-                    clipRule="evenodd"
-                  />
-                  <path
-                    fillRule="evenodd"
-                    d="M1 10a.75.75 0 0 1 .75-.75h9.546l-1.048-.943a.75.75 0 1 1 1.004-1.114l2.5 2.25a.75.75 0 0 1 0 1.114l-2.5 2.25a.75.75 0 1 1-1.004-1.114l1.048-.943H1.75A.75.75 0 0 1 1 10Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+          <div className="col-start-2 max-sm:mt-36 max-sm:col-start-1 max-sm:row-start-3 max-sm:col-span-2  col-span-1 flex flex-col  max-w-xl items-center justify-center ">
+            <div className="flex  gap-3 py-4   justify-center items-center max-w-[300px] w-full  flex-col">
+              <div className="w-full text-[#646669] px-2 flex justify-start items-center  gap-3 font-mono ">
+                <div className="">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M17 4.25A2.25 2.25 0 0 0 14.75 2h-5.5A2.25 2.25 0 0 0 7 4.25v2a.75.75 0 0 0 1.5 0v-2a.75.75 0 0 1 .75-.75h5.5a.75.75 0 0 1 .75.75v11.5a.75.75 0 0 1-.75.75h-5.5a.75.75 0 0 1-.75-.75v-2a.75.75 0 0 0-1.5 0v2A2.25 2.25 0 0 0 9.25 18h5.5A2.25 2.25 0 0 0 17 15.75V4.25Z"
+                      clipRule="evenodd"
+                    />
+                    <path
+                      fillRule="evenodd"
+                      d="M1 10a.75.75 0 0 1 .75-.75h9.546l-1.048-.943a.75.75 0 1 1 1.004-1.114l2.5 2.25a.75.75 0 0 1 0 1.114l-2.5 2.25a.75.75 0 1 1-1.004-1.114l1.048-.943H1.75A.75.75 0 0 1 1 10Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+                <div>login</div>
               </div>
-              <div>login</div>
+              <input
+                type="text"
+                aria-label=""
+                className="w-full text-[#d1d0c5]   focus:outline-2 focus:outline-amber-200   px-2 py-2  font-mono placeholder:text-[#696a6c] bg-[#2C2E31] p-1 rounded-sm "
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="email"
+              />
+              <input
+                type="text"
+                aria-label=""
+                className="w-full text-[#d1d0c5]    focus:outline-2 focus:outline-amber-200  px-2 py-2  font-mono placeholder:text-[#696a6c] bg-[#2C2E31] p-1 rounded-sm "
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="password"
+              />
+              <button
+                onClick={handleLogin}
+                className="w-full cursor-pointer hover:bg-[#d1d0c5] hover:text-[#323437] py-1.5  text-[#646669] flex justify-center items-center gap-2   px-2   font-mono  bg-[#35383c] p-1 rounded-sm " // onClick={handleLogin}
+              >
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M17 4.25A2.25 2.25 0 0 0 14.75 2h-5.5A2.25 2.25 0 0 0 7 4.25v2a.75.75 0 0 0 1.5 0v-2a.75.75 0 0 1 .75-.75h5.5a.75.75 0 0 1 .75.75v11.5a.75.75 0 0 1-.75.75h-5.5a.75.75 0 0 1-.75-.75v-2a.75.75 0 0 0-1.5 0v2A2.25 2.25 0 0 0 9.25 18h5.5A2.25 2.25 0 0 0 17 15.75V4.25Z"
+                      clipRule="evenodd"
+                    />
+                    <path
+                      fillRule="evenodd"
+                      d="M1 10a.75.75 0 0 1 .75-.75h9.546l-1.048-.943a.75.75 0 1 1 1.004-1.114l2.5 2.25a.75.75 0 0 1 0 1.114l-2.5 2.25a.75.75 0 1 1-1.004-1.114l1.048-.943H1.75A.75.75 0 0 1 1 10Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+                sign in
+              </button>
             </div>
-            <input
-              type="text"
-              aria-label=""
-              className="w-full text-[#d1d0c5]   focus:outline-2 focus:outline-amber-200   px-2 py-2  font-mono placeholder:text-[#696a6c] bg-[#2C2E31] p-1 rounded-sm "
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="email"
-            />
-            <input
-              type="text"
-              aria-label=""
-              className="w-full text-[#d1d0c5]    focus:outline-2 focus:outline-amber-200  px-2 py-2  font-mono placeholder:text-[#696a6c] bg-[#2C2E31] p-1 rounded-sm "
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="password"
-            />
-            <button
-              onClick={handleLogin}
-              className="w-full cursor-pointer hover:bg-[#d1d0c5] hover:text-[#323437] py-1.5  text-[#646669] flex justify-center items-center gap-2   px-2   font-mono  bg-[#35383c] p-1 rounded-sm " // onClick={handleLogin}
-            >
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="size-5"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M17 4.25A2.25 2.25 0 0 0 14.75 2h-5.5A2.25 2.25 0 0 0 7 4.25v2a.75.75 0 0 0 1.5 0v-2a.75.75 0 0 1 .75-.75h5.5a.75.75 0 0 1 .75.75v11.5a.75.75 0 0 1-.75.75h-5.5a.75.75 0 0 1-.75-.75v-2a.75.75 0 0 0-1.5 0v2A2.25 2.25 0 0 0 9.25 18h5.5A2.25 2.25 0 0 0 17 15.75V4.25Z"
-                    clipRule="evenodd"
-                  />
-                  <path
-                    fillRule="evenodd"
-                    d="M1 10a.75.75 0 0 1 .75-.75h9.546l-1.048-.943a.75.75 0 1 1 1.004-1.114l2.5 2.25a.75.75 0 0 1 0 1.114l-2.5 2.25a.75.75 0 1 1-1.004-1.114l1.048-.943H1.75A.75.75 0 0 1 1 10Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </div>
-              sign in
-            </button>
           </div>
         </div>
       </div>
